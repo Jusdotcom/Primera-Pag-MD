670138ff4bf385240a6ce85c083b9aa68993cad5
Jusdotcom <jusdotcom162525@gmail.com>
2024-06-26 13:47:19 -0600

    prueba 1

---
+++ diseno.js
@@ -0,0 +1,8 @@
+function muestraOculta(id){
+    let div = document.getElementById(id);
+    if (div.style.display == "none"){
+        div.style.display = "flex"
+    } else{
+        div.style.display = "none";
+    }
+}
